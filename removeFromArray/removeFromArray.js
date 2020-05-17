d05e3e740de7990fb5dd885d00dead8fd58b4337
briighter <jaylima0@gmail.com>
2020-05-17 17:44:59 -0400

    Update removeFromArray solution. Note: One test failed.

--- removeFromArray/removeFromArray.js
+++ removeFromArray/removeFromArray.js
@@ -1,5 +1,12 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(arr) {
+    for (let i = 0; i <= arr.length; i++) {
+        for (let j = 1; j <= arguments.length; j++) {
+            if (arr[i] === arguments[j]) {
+                arr.splice(i, 1);
+            }
+        }
+    }
+    return arr;
 }
 
 module.exports = removeFromArray
