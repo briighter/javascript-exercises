00e6cf55735115efe50b268b07587f6cf2b43db6
briighter <jaylima0@gmail.com>
2020-05-17 20:14:53 -0400

    Update sum all numbers solution. Note: Return ERROR for non-number parameters is not solved

--- sumAll/sumAll.js
+++ sumAll/sumAll.js
@@ -1,5 +1,23 @@
-const sumAll = function() {
+const sumAll = function(x, y) {
+    if ((isNaN(x) || isNaN(y)) || ((x || y) < 0)) {
+        return "ERROR"
+    } else if (x < y) {
+        sum = x;
+        tracker = x + 1;
+        while (tracker <= y) {
+            sum+=tracker
+            tracker++
+        }
+    } else if (x > y) {
+        sum = y;
+        tracker = y + 1;
+        while (tracker <= x) {
+            sum+=tracker
+            tracker++
+        }
+    }
 
+    return sum;
 }
 
 module.exports = sumAll
