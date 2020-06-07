c37b560a313fcb4db15cae545b8f3b52a1d01d8b
Jason <jaylima0@gmail.com>
2020-06-07 16:48:32 -0400

    Add factorial solution

--- calculator/calculator.js
+++ calculator/calculator.js
@@ -1,12 +1,12 @@
-function add (x, y) {
+function add(x, y) {
 	return x + y;
 }
 
-function subtract (x, y) {
+function subtract(x, y) {
 	return x - y;
 }
 
-function sum (arr) {
+function sum(arr) {
 	var finalSum = 0;
 
 	for (i = 0; i < arr.length; i++) {
@@ -16,7 +16,7 @@ function sum (arr) {
 	return finalSum;
 }
 
-function multiply (arr) {
+function multiply(arr) {
 	var finalProduct = 1;
 
 	for (i = 0; i < arr.length; i++) {
@@ -30,8 +30,12 @@ function power(x, y) {
 	return Math.pow(x, y);
 }
 
-function factorial() {
-	
+function factorial(n) {
+	if (n == 0 || n == 1) {
+		return 1
+	} else {
+		return n * factorial(n - 1)
+	}	
 }
 
 module.exports = {
@@ -39,6 +43,6 @@ module.exports = {
 	subtract,
 	sum,
 	multiply,
-    power,
+	power,
 	factorial
 }
